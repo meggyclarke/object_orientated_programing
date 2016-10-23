f794dc1bdbbbb3baf5fbb08f845db8832efb3463
Meg Clarke <meggyclarke@gmail.com>
2016-10-22 20:08:42 -0400

    Rovers have personal Space

--- rover.rb
+++ rover.rb
@@ -14,13 +14,6 @@ class Rover
     puts "#{@direction}, its a beautiful shade of red."
   end
 
-  #def print_position
-    #puts "inside print position"
-    #puts @x_coordinate
-    #puts @y_coordinate
-    #puts @direction
-  #end
-
   def read_instruction (instructions)
     #puts "inside read instruction"
     #puts instructions
@@ -104,7 +97,32 @@ class Rover
     #        abort "Your rover2 is ontop of rover1! Yikes get a drivers licence we are cutting you off!"
     #    end
     #end
+
+    def personal_space(input1, input2)
+        #if  user_x2 = @takenx || user_y2 = @takeny
+        if input1 = input2 #|| inputy = @rover1_newspoty
+          abort "Your rover2 is ontop of rover1! Yikes get a drivers licence we are cutting you off!"
+       end
+    end
 end #end of class
+
+#class Where_i_stand
+#    attr_accessor :takenx
+#    attr_accessor :takeny
+
+#    def initialize
+#        @takenx = []
+#        @takeny = []
+#    end
+
+#    def personal_space_x(productx)
+#        @takenx << productx
+#    end
+#    def personal_space_y(producty)
+#        @takeny << producty
+#    end
+
+#end
 
 
 puts " We assume the direction you start is your North (like 12 O Clock).
@@ -128,7 +146,11 @@ instructions = gets.chomp.upcase.to_s
 rover1.read_instruction(instructions)
 
 puts "You are now checking out the sights at #{rover1.x_coordinate}, #{rover1.y_coordinate}. Mars is Fun!"
-
+@rover1_newspotx = rover1.x_coordinate
+@rover1_newspoty = rover1.y_coordinate
+#
+# @rover1_newspotx.max_grid
+# @rover1_newspoty.max_grid
 puts "Please enter your starting x co-ordinate for the second Rover.
 We assume the direction you start is your North (like 12 O Clock)."
 user_x2= gets.chomp.to_i
@@ -139,17 +161,20 @@ user_y2 = gets.chomp.to_i
 rover2 = Rover.new("rover2", user_x2, user_y2, "N")
 rover2.max_grid#.personal_space
 
-#def personal_space
-#    takenx =  rover1.x_coordinate
-#    takeny = rover1.y_coordinate
-#        if  user_x2 = takenx || user_y2 = takeny
-#       abort "Your rover2 is ontop of rover1! Yikes get a drivers licence we are cutting you off!"
- #  end
-#end
+#spot_takenx = Where_i_stand.new
+#spot_takenx.personal_space_x("#{rover1.x_coordinate}")
+
+#spot_takeny = Where_i_stand.new
+#spot_takeny.personal_space_y("#{rover1.y_coordinate}")
+
+@rover2_spotx = rover2.x_coordinate
+@rover2_spoty = rover2.y_coordinate
+
 
 rover2.read_instruction(instructions)
 
-#rover2.personal_space
+rover2.personal_space(@rover1_newspoty, @rover2_spotx)
+rrover2.personal_space(@rover2_spoty, @over2_spoty)
 
 puts "Your second Rover is now gloriously gazing at #{rover2.x_coordinate}, #{rover2.y_coordinate}.
 Thats the end of your gets, thanks for visiting Mars."
